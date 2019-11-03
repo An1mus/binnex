cb8ee05b5b7d6c79de027a75e377218a4517ff7d
an1mus <1stallways@gmail.com>
2019-11-03 18:14:07 +0200

    initial clean up

---
+++ server/test.js
@@ -0,0 +1,7 @@
+someFunc();
+
+function someFunc(){ console.log('someFunc'); }
+var someOtherFunc = () => {console.log('someOtherFunc');};
+const someNewFunc = function () {console.log('someNewFunc');};
+
+someOtherFunc();
