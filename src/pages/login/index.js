5ae9d3da66f5b9603bd0f3a866afeb92112676e4
an1mus <1stallways@gmail.com>
2019-11-03 18:18:19 +0200

    Login component init

---
+++ src/pages/login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+	render() {
+		return (
+			<>
+				<input type="text"/>
+				<input type="text"/>
+				<button>Login</button>
+			</>
+		);
+	}
+}
