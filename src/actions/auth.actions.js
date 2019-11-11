b56f7e76d7c72435b75ef970cd4c906b2a3323ee
an1mus <1stallways@gmail.com>
2019-11-11 13:31:31 +0200

    Login action reator update

--- src/actions/auth.actions.js
+++ src/actions/auth.actions.js
@@ -1,17 +1,45 @@
 import LOGIN_ACTIONS from "./types/login";
+import authService from "../services/auth.service";
+import {history} from "../helpers";
 
-const userActions = {
+export const userActions = {
 	login,
 };
 
 function login(username, password) {
 	return dispatch => {
-		function onSuccess(user){
-			dispatch({type: LOGIN_ACTIONS.LOGIN_SUCCESS, payload: user})
+		function onSuccess(user) {
+			dispatch({
+				type: LOGIN_ACTIONS.LOGIN_SUCCESS,
+				payload: {
+					user,
+					isLoggedIn: true,
+					isLoggingIn: false
+				}
+			});
+			history.push('/');
 		}
 
 		function onFailure(user) {
-			dispatch({type: LOGIN_ACTIONS.LOGIN_FAILURE, payload: user});
+			dispatch({
+				type: LOGIN_ACTIONS.LOGIN_FAILURE,
+				payload: {
+					user,
+					isLoggingIn: false
+				}
+			});
+		}
+
+		function onRequest() {
+			dispatch({type: LOGIN_ACTIONS.LOGIN_REQUEST, payload: {isLoggingIn: true}})
+		}
+
+		onRequest();
+		try {
+			const response = authService.login(username, password);
+			onSuccess(response);
+		} catch (e) {
+			onFailure();
 		}
 	}
 }
