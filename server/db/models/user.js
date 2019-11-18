0545fad12ebf89a0fb5c40bc2832b16a871b476b
an1mus <1stallways@gmail.com>
2019-11-18 22:08:36 +0200

    User schema

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,5 +1,26 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-
+	username: {
+		type: String,
+		lowercase: true,
+		unique: true,
+		index: true,
+		required: [true, 'username can\'t be blank' ],
+		match: [/^[a-zA-Z0-9]+$/, 'username is invalid'],
+	},
+	password: {
+		type: String,
+		lowercase: true,
+		unique: true,
+		index: true,
+		required: [true, 'password can\'t be blank' ],
+		match: [/\S+@\S+\.\S+/, 'password is invalid'],
+	},
+	email: String,
+	avatar: String,
+	bio: String,
+	salt: String,
 });
+
+mongoose.model('User', UserSchema);
